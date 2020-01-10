6d1d0266022db1fe3f462814785ebd7080ebb01f
tkillestein <thomas.killestein@gmail.com>
2020-01-10 13:58:35 +0000

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='goto_astromtools',
+      version='1.0',
+      description='Astrometry utils for GOTO',
+      url='https://github.com/tkillestein/goto_astromtools',
+      author='Thomas Killestein',
+      author_email='t.killestein@warwick.ac.uk',
+      license='MIT',
+      packages=['goto_astromtools'],
+      zip_safe=False)
