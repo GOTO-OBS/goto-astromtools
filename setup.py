42ad5a8527db0e1f7a797301b0f5eba74ce1d9ce
Thomas Killestein <thomas.killestein@gmail.com>
2021-07-02 11:43:48 +0100

    update repo URL in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='goto_astromtools',
       version='1.0',
       description='Astrometry utils for GOTO',
-      url='https://github.com/tkillestein/goto_astromtools',
+      url='https://github.com/GOTO-OBS/goto_astromtools',
       author='Thomas Killestein',
       author_email='t.killestein@warwick.ac.uk',
       license='MIT',
